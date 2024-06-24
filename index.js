deca2f518cb0cab2302d6eb77eb611b16e540ebb
Johnson Omwoyo <johnsonomwoyo100@gmail.com>
2024-06-24 13:22:34 +0300

    array methods

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+
+let cats = [];
+cats.length = 0;
+cats.push("Milo", "Otis", "Garfield");
+
+function destructivelyAppendCat(name) {
+  //.push
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  //.unshift
+  cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+  //pop
+  cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+  //shift
+  cats.shift();
+}
+function appendCat(name) {
+  return [...cats.slice(0), name];
+}
+function prependCat(name) {
+  return [name, ...cats.slice(0)];
+}
+function removeLastCat() {
+  return [...cats.slice(0, cats.length - 1)];
+}
+function removeFirstCat() {
+  return [...cats.slice(1)];
+}
